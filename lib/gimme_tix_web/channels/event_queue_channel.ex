6b63de5efbb3f8513ce779af47e976ac40445508
Preston Mueller <preston.mueller@me.com>
2019-08-22 00:07:05 -0400

    Refactor some of the event channel. Use aliases to prevent so many module names all over the place. Make the counting of number of users for an event happen in the database instead of loading the whole set of users and then finding the length

--- lib/gimme_tix_web/channels/event_queue_channel.ex
+++ lib/gimme_tix_web/channels/event_queue_channel.ex
@@ -1,50 +1,50 @@
 defmodule GimmeTixWeb.EventQueueChannel do
+  alias GimmeTix.User
+  alias GimmeTix.Event
+  alias GimmeTix.Repo
   use GimmeTixWeb, :channel
   import Ecto.Query, only: [from: 2]
 
   def join("event_queue:" <> event_id, payload, socket) do
-    if authorized?(payload) do
-      event = GimmeTix.Repo.get!(GimmeTix.Event, event_id)
-      socket = assign(socket, :event, event)
-      # race condition here because reading and then inserting isn't atomic
-      pos_in_queue = length(from(u in GimmeTix.User, where: u.event_id == ^event_id) |> GimmeTix.Repo.all())# |> GimmeTix.Repo.aggregate(:count, :id)
-      IO.puts(pos_in_queue)
-      {:ok, user} =  GimmeTix.User.changeset(%GimmeTix.User{}, %{event_id: event_id, pos_in_queue: pos_in_queue, uuid: UUID.uuid4()})
-                      |> GimmeTix.Repo.insert()
-      socket = assign(socket, :user, user)
-      response = %{current_user: event.current_user, uuid: user.uuid, pos_in_queue: user.pos_in_queue}
-      {:ok, response, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+    event = get_event(event_id)
+    socket = assign(socket, :event, event)
+    # race condition here because reading and then inserting isn't atomic
+    pos_in_queue = Repo.one(from u in User, where: u.event_id == ^event_id, select: count(u.id))
+    # also need to deal with weird corner cases like current_user being way higher than pos_in_queue
+    {:ok, user} =  User.changeset(%User{}, %{event_id: event_id, pos_in_queue: pos_in_queue, uuid: UUID.uuid4()})
+                    |> Repo.insert()
+    socket = assign(socket, :user, user)
+    response = %{current_user: event.current_user, uuid: user.uuid, pos_in_queue: user.pos_in_queue}
+    send(self, :after_join)
+    {:ok, response, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_info(:after_join, socket) do
+    event = get_event(socket.assigns[:event].id)
+    push socket, "new_current_user", %{current_user: event.current_user}
+    {:noreply, socket}
   end
+
 
   def handle_in("buy", payload, socket) do
-    event = GimmeTix.Repo.get!(GimmeTix.Event, socket.assigns[:event].id)
-    {:ok, event} =  GimmeTix.Web.Event.changeset(event, %{current_user: event.current_user + 1})
-                    |> GimmeTix.Repo.update()
+    event = get_event(socket.assigns[:event].id)
+    {:ok, event} =  Event.changeset(event, %{current_user: event.current_user + 1})
+                    |> Repo.update()
     socket = assign(socket, :event, event)
     broadcast socket, "new_current_user", %{current_user: event.current_user}
     {:noreply, socket}
   end
 
   def handle_in("pass", payload, socket) do
-    event = GimmeTix.Repo.get!(GimmeTix.Event, socket.assigns[:event].id)
-    {:ok, event} =  GimmeTix.Web.Event.changeset(event, %{current_user: event.current_user + 1})
-                    |> GimmeTix.Repo.update()
+    event = get_event(socket.assigns[:event].id)
+    {:ok, event} =  Event.changeset(event, %{current_user: event.current_user + 1})
+                    |> Repo.update()
     socket = assign(socket, :event, event)
     broadcast socket, "new_current_user", %{current_user: event.current_user}
     {:noreply, socket}
   end
 
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+  defp get_event(event_id) do
+    Repo.get!(Event, event_id)
   end
 end
